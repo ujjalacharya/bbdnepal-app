a2bb027feb628665195ec0c310d0c061da265af5
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-03-01 17:27:53 +0545

    add drawer navigator

--- App.js
+++ App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-import {createDrawerNavigator} from 'react-navigation';
+import {createDrawerNavigator, createAppContainer } from 'react-navigation';
+
+import { Font, AppLoading } from "expo";
+
+import MainScreen from './Components/MainScreen';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
+
+  async componentWillMount() {
+    await Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+    });
+    this.setState({ loading: false });
+  }
+
   render() {
+    if (this.state.loading) {
+      return (
+          <AppLoading />
+      );
+    }
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+        <DrawerNav />
     );
   }
 }
 
+const RootDrawerNavigator = createDrawerNavigator({
+  Main: MainScreen,
+},{
+  initialRouteName: 'Main',
+})
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const DrawerNav = createAppContainer(RootDrawerNavigator);
