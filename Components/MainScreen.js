0d697af37b0e22f65347f1538eeb1002bebb6dc8
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-03-01 18:46:57 +0545

    add dummy main screen

--- Components/MainScreen.js
+++ Components/MainScreen.js
@@ -1,22 +1,44 @@
-import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native';
-
-
+import React, { Component } from "react";
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Left,
+  Right,
+  Body,
+  Icon,
+  Text
+} from "native-base";
 export default class MainScreen extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text> Main Screen </Text>
-      </View>
-    )
+        <Container>
+          <Header>
+            <Left>
+              <Button transparent>
+                <Icon name="menu" />
+              </Button>
+            </Left>
+            <Body>
+              <Title>Header</Title>
+            </Body>
+            <Right />
+          </Header>
+          <Content>
+            <Text>This is Content Section</Text>
+          </Content>
+          <Footer>
+            <FooterTab>
+              <Button full>
+                <Text>Footer</Text>
+              </Button>
+            </FooterTab>
+          </Footer>
+        </Container>
+    );
   }
 }
-
-const styles = StyleSheet.create({
- container: {
-   flex: 1,
-   backgroundColor: '#fff',
-   alignItems: 'center',
-   justifyContent: 'center',
- },
-});
