22ee43e4ee04814474d9e5693faa7e7f8270895c
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-03-07 12:34:31 +0545

    make contact form field block

--- Components/ContactUsScreen/ContactForm.js
+++ Components/ContactUsScreen/ContactForm.js
@@ -22,25 +22,20 @@ export default class FormExample extends Component {
           <Content>
            <Text style={styles.title}>Contact Us</Text>
             <Form>
-              <Item floatingLabel>
-                <Label>First Name*</Label>
-                <Input />
+              <Item regular last style={styles.input}>
+                <Input placeholder="First Name*"/>
               </Item>
-              <Item floatingLabel last>
-                <Label>Last Name*</Label>
-                <Input />
+              <Item regular last style={styles.input}>
+                <Input placeholder="Last Name*"/>
               </Item>
-              <Item floatingLabel last>
-                <Label>Email*</Label>
-                <Input />
+              <Item regular last style={styles.input}>
+                <Input placeholder="Email*"/>
               </Item>
-              <Item floatingLabel last>
-                <Label>Phone Number*</Label>
-                <Input />
+              <Item regular last style={styles.input}>
+                <Input placeholder="Phone Number*"/>
               </Item>
-              <Item floatingLabel last>
-                <Label>Subject</Label>
-                <Input />
+              <Item regular last style={styles.input}>
+                <Input placeholder="Subject"/>
               </Item>
                 <Textarea style={{marginTop: 20}} rowSpan={5} bordered placeholder="Message" />
               <View style={{ marginTop: 10, flex: 1, padding: 10 }}>
@@ -73,5 +68,8 @@ const styles = StyleSheet.create({
  },
  contact: {
   flex: 1
+ },
+ input:{
+  margin: 10
  }
 });
