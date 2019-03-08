cb1913ca2b9e47c08c6096cd110c57e050908bc6
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-03-08 13:26:24 +0545

    add multiple members on about us

--- Components/AboutUsScreen/Members.js
+++ Components/AboutUsScreen/Members.js
@@ -17,10 +17,38 @@ class Members extends Component {
           />
         </CardItem>
         <CardItem>
-        <Text style={{fontSize: 15}}>Ramhari Dhital</Text>
+        <Text style={{fontSize: 20}}>Ramhari Dhital</Text>
         </CardItem>
         <CardItem>
           <Text>Ramhari Dhital is known as a success man in finance industry in nepal. who protects his client’s interests as though they were his own.</Text>
+        </CardItem>
+        </Content>
+        <Content>
+        <CardItem cardBody>
+          <Image
+            source={require("../../assets/images/member2.jpg")}
+            style={styles.image}
+          />
+        </CardItem>
+        <CardItem>
+        <Text style={{fontSize: 20}}>Mrs. Tulsi Dhital</Text>
+        </CardItem>
+        <CardItem>
+          <Text>Mrs. Tulsi Dhital  is responsible for the day to day operations or an organization. She could be thought of as the second in command behind the Chairman or CEO.</Text>
+        </CardItem>
+        </Content>
+        <Content>
+        <CardItem cardBody>
+          <Image
+            source={require("../../assets/images/member3.jpg")}
+            style={styles.image}
+          />
+        </CardItem>
+        <CardItem>
+        <Text style={{fontSize: 20}}>Ramhari Dhital</Text>
+        </CardItem>
+        <CardItem>
+          <Text>James M. Beck was a member of the Republican Party, who served as U.S. Solicitor General and U.S. Representative from Pennsylvania.</Text>
         </CardItem>
         </Content>
       </Card>
