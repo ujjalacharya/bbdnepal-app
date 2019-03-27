aec7a099f4592078c8c02dd074ce22dd526bb535
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-03-27 19:48:02 +0545

    add ourserive dummy data

--- Components/OurServiceScreen.js
+++ Components/OurServiceScreen.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
-import { Container } from "native-base";
+import { Container, Content, Accordion } from "native-base";
 import Head from "./shared/Head";
+
+const dataArray = [
+  { title: "Serivice 1", content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae omnis facilis consectetur doloremque deserunt alias iure soluta hic accusamus, ea sequi reprehenderit beatae aspernatur ut debitis cum necessitatibus neque ducimus." },
+  { title: "Service 2", content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae omnis facilis consectetur doloremque deserunt alias iure soluta hic accusamus, ea sequi reprehenderit beatae aspernatur ut debitis cum necessitatibus neque ducimus."  },
+  { title: "Serivice 3", content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae omnis facilis consectetur doloremque deserunt alias iure soluta hic accusamus, ea sequi reprehenderit beatae aspernatur ut debitis cum necessitatibus neque ducimus." },
+  { title: "Service 4", content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae omnis facilis consectetur doloremque deserunt alias iure soluta hic accusamus, ea sequi reprehenderit beatae aspernatur ut debitis cum necessitatibus neque ducimus."  },
+];
 
 class OurServiceScreen extends Component {
 
@@ -12,6 +19,9 @@ class OurServiceScreen extends Component {
     return (
       <Container>
         <Head {...this.props}/>
+        <Content padder>
+        <Accordion dataArray={dataArray} icon="ios-add" expandedIcon="ios-remove" expanded={true} animation={true}/>
+      </Content>
       </Container>
     )
   }
