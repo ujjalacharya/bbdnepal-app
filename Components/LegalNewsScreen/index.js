0bf3cc59acf716f680c5176f64f9966d9741f1af
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-03-28 23:23:00 +0545

    add loader at legalnews screen

--- Components/LegalNewsScreen/index.js
+++ Components/LegalNewsScreen/index.js
@@ -5,6 +5,7 @@ import LegalCard from "./SharedCard";
 import { ScrollView } from "react-native-gesture-handler";
 import Axios from "axios";
 import { RefreshControl } from "react-native";
+import Loading from "../shared/Loading";
 
 class LegalNewsScreen extends Component {
   static navigationOptions = {
@@ -13,28 +14,9 @@ class LegalNewsScreen extends Component {
 
   state = {
     heading: "Legal News",
-    cards: [
-      {
-        title: "Ramhari Dhital",
-        image: "https://i.ibb.co/WgR4yJH/member1.jpg",
-        desc:
-          "Ramhari Dhital is known as a success man in finance industry in nepal. who protects his client’s interests as though they were his own."
-      },
-      {
-        title: "Mrs. Tulsi Dhital",
-        image: "https://i.ibb.co/Q6v3tDF/member2.jpg",
-        desc:
-          "Mrs. Tulsi Dhital  is responsible for the day to day operations or an organization. She could be thought of as the second in command behind the Chairman or CEO."
-      },
-      {
-        title: "Mr. Tej Bdr. Karki",
-        image: "https://i.ibb.co/vJhCQpH/member3.png",
-        desc:
-          "James M. Beck was a member of the Republican Party, who served as U.S. Solicitor General and U.S. Representative from Pennsylvania."
-      }
-    ],
     data: [],
-    refreshing: false
+    refreshing: false,
+    loading: true
   };
 
   componentDidMount() {
@@ -43,12 +25,12 @@ class LegalNewsScreen extends Component {
 
   callAPI = () => {
     Axios.get("http://bbdnepal.com/wp-json/wp/v2/posts").then(resp => {
-      this.setState({ data: resp.data });
+      this.setState({ data: resp.data, loading: false });
     });
   };
 
   onRefresh = () => {
-    this.setState({ data: [] });
+    this.setState({ data: [], loading: true });
     setTimeout(() => {
       this.callAPI();
     }, 1000);
@@ -68,11 +50,11 @@ class LegalNewsScreen extends Component {
           }
         >
           <Content>
-            <LegalCard
+            {this.state.loading ? (<Loading size="large"/>) : (<LegalCard
               heading={this.state.heading}
               cards={this.state.cards}
               data={this.state.data}
-            />
+            />)}
           </Content>
         </ScrollView>
       </Container>
